05028d6b2dd6fee291a6f57e466f6c1c7dee1877
Andrea Galeazzi <andrea.galeazzi@polimi.it>
2022-04-06 17:20:09 +0200

    refactor random generators

--- src/random/randomGenerator.py
+++ src/random/randomGenerator.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 
-def generateRandomIngredient(low, high, size=1, seedInt=69):
+def generateRandomInt(low, high, size=1, seedInt=69):
   rng = np.random.default_rng(random.seed(seedInt))
   randIngredient = rng.integers(low, high, size)
   return int(randIngredient)
@@ -13,7 +13,7 @@ def generateUniqueRandomIngredient(listGroupIngredients, low, high, size=1):
     try:
       listGroupIngredients.index(randIngredient)
       equalFlag = True
-      randIngredient = generateRandomIngredient(low, high, size)
+      randIngredient = generateRandomInt(low, high, size)
     except ValueError:
       equalFlag = False
   return int(randIngredient)
